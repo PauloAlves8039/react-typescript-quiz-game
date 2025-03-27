416ee9d5957df9078c81b4d6c41577deac5fc64d
PauloAlves8039 <pj38alves@gmail.com>
2025-03-27 19:37:26 -0300

    feat: aplica funções reducer ao context

--- src/context/QuizContext.tsx
+++ src/context/QuizContext.tsx
@@ -1,8 +1,13 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useReducer } from "react"
+
+type Status = "idle" | "fetching" | "ready";
 
 interface QuizState {
-    gameStatus: "idle" | "fetching" | "ready"
+    gameStatus: Status
 };
+
+type QuizAction = 
+    { type: "setStatus"; payload: Status };
 
 const initialState: QuizState = {
     gameStatus: "idle"
@@ -11,7 +16,7 @@ const initialState: QuizState = {
 const QuizContext = createContext<QuizState>(initialState);
 
 export function QuizProvider({ children }: { children: React.ReactElement }) {
-    const [state, setState] = useState(initialState);
+    const [state, dispatch] = useReducer(QuizReducer, initialState);
 
     return (
         <QuizContext.Provider value={state}>
@@ -22,4 +27,13 @@ export function QuizProvider({ children }: { children: React.ReactElement }) {
 
 export function useQuiz() {
     return useContext(QuizContext);
+}
+
+function QuizReducer(state: QuizState, action: QuizAction): QuizState {
+    switch (action.type) {
+        case "setStatus":
+            return {...state, gameStatus: action.payload};
+        default:
+            throw new Error("Unknown action");
+    }
 }
