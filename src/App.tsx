f52d1c7ecb1a64d537efc253741b390b60ba6fde
PauloAlves8039 <pj38alves@gmail.com>
2025-03-28 11:37:59 -0300

    feat: obtém perguntas da API do Trivia

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import "./App.scss";
+import {useEffect} from "react";
 import Game from "./components/Gamer/Game";
 import Score from "./components/Score/Score";
 import { useQuiz } from "./context/QuizContext";
+import "./App.scss";
 
 export default function App() {
 
-  const {state, dispatch} = useQuiz();
+  const {state} = useQuiz();
   console.log(state);
+
+  useEffect(() => {
+    if(state.gameStatus == "idle"){
+      fetchQuestion();
+    }
+  });
+
+  async function fetchQuestion() {
+    const response = await fetch("https://opentdb.com/api.php?amount=1&category=18");
+    let data = await(response.json());
+    let question = data.results[0];
+    console.log("Data",data);
+  };
 
   return (
     <>
