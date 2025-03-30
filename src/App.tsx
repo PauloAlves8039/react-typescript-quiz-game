95b59274c9d318cee89e788c2220e174d806328c
PauloAlves8039 <pj38alves@gmail.com>
2025-03-30 10:50:33 -0300

    feat: define repostas corretas e incorretas

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,9 @@ export default function App() {
       if (data.response_code === success) {
         let question: Question = data.results[0];
 
+        let randomIndex = Math.round(Math.random() * question.incorrect_answers.length);
+        question.incorrect_answers.splice(randomIndex, 0, question.correct_answer);
+
         dispatch({ type: "setStatus", payload: "ready" });
         dispatch({ type: "setQuestion", payload: question });
         
