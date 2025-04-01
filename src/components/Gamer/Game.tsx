b88878f5365aada693392e0c7429665780d3279a
PauloAlves8039 <pj38alves@gmail.com>
2025-04-01 14:59:06 -0300

    feat: cria botão para a próxima pergunta

--- src/components/Gamer/Game.tsx
+++ src/components/Gamer/Game.tsx
@@ -27,7 +27,10 @@ export default function Game() {
 
                 {
                     state.gameStatus == "answered" &&
-                    <Result />
+                    <>
+                        <Result />
+                        <button onClick={() => {dispatch({type: "setStatus", payload: "idle"})}}>Next Question</button>
+                    </>
                 }
 
             </div>
